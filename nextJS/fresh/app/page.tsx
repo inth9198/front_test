90bd12f3dc5da322ef6782b2e4a851a6cd7a7d77
inth9198 <inth2474@naver.com>
2023-05-10 18:53:00 +0900

    [feat]first commit

---
+++ nextJS/fresh/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="title">쿠팡 fresh</div>
+      <div className="sub-title">let's go</div>
+    </div>
+  );
+}
---
+++ nextJS/fresh/theme.ts
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  // 여기에 테마 설정을 추가할 수 있습니다.
+});
+
+export default theme;
